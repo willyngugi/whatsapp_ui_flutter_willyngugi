157b8e423a08b799c5c6d32c1de8a406602ee383
willyngugi <willteksolutionsofficial@gmail.com>
2024-09-25 19:15:39 +0300

    more UI changes

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Navigation Demo',
+      title: 'Whatsapp UI',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
